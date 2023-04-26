2fb80bbd297f92f69ac0c90bdc6f4006014f1c90
rumplestilzken <rumplestilzken@gmail.com>
2023-04-26 16:29:58 -0400

    Updated rm with sudo to avoid prompt

--- process_gargoyle.sh
+++ process_gargoyle.sh
@@ -10,18 +10,30 @@ fi
 echo "Starting vnkdlite process."
 sudo bash ./lite-adapter.sh 64 $srcFile
 
-base_name="$(basename -- "$srcFile")"
-new_base_name=${base_name%.*}
-extension=${base_name##*.}
+base_name="$(basename -- "$srcFile")" #Full File Name
+new_base_name=${base_name%.*} #No extentsion
+base_path=${srcFile%$base_name} #Original Path
+extension=${base_name##*.} #Extension
 new_name="$new_base_name-vndklite.$extension"
 
-echo "Renaming ''$base_name' to '$new_name' "
-
-if [ -f s.img]; then
-  mv s.img $new_base_name
+if [ -f s.img ]; then
+  echo "Renaming 's.img' to '$new_name' "
+  mv s.img $new_name
 else
-  echo "Renaming failed."
+  echo "s.img not found."
+  exit
 fi
 
-echo "Archiving '$new_name'"
-tar -czvf "$new_base_name.tar.gz"
+archive_name="$new_name.tar.gz"
+echo "Archiving '$new_name' to '$archive_name'"
+tar -czvf "$archive_name" "$new_name"
+
+echo "Moving '$archive_name' to '$base_path$new_name'"
+if [ -f $archive_name ]; then
+  mv $archive_name "$base_path$archive_name"
+  sudo rm $new_name
+else
+  echo "An error occured in moving the file"
+fi
+
+echo "Process Complete."
